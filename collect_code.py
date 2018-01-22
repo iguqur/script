38be2b23365d3bac8808d1a11bdd7f0df827e233
pengjiali <pengjiali@standard-robots.com>
2018-01-22 21:15:14 +0800

    Add：将文件夹下的所有代码，拼接到一个文件中

---
+++ collect_code.py
@@ -0,0 +1,21 @@
+import os
+
+def collectCode(dirPath, outFilePath):
+    '''将文件夹下的所有代码，拼接到一个文件中'''
+    doc = open(outFilePath, "a", encoding='UTF-8')
+
+    list = os.listdir(dirPath)
+    for i in range(0, len(list)):
+        path = os.path.join(dirPath, list[i])
+        if os.path.isfile(path):
+            if path.endswith("cpp") or path.endswith("h"):
+                print(path)
+                file = open(path, 'r', encoding='UTF-8')
+                for eachLine in file:
+                    doc.write(eachLine)
+                file.close()
+        elif os.path.isdir(path):
+            collectCode(path, outFilePath)
+
+if __name__ == '__main__':
+    collectCode("C:\workspace\Massif\MassifClient", "I:\\a.txt")
