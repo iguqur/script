788328281e1a331933f725db171f7c7c30f35d90
pengjiali <pengjiali@standard-robots.com>
2018-11-26 15:59:16 +0800

    Change:

--- tcp_client.py
+++ tcp_client.py
@@ -1,14 +1,21 @@
 import socket
 
-HOST = '192.168.83.24'    # The remote host
+HOST = '192.168.71.115'    # The remote host
 PORT = 5003             # The same port as used by the server
 
-sendDataStr = '22 33'
+sendDataStr = '68 05 00 00 04 06 11 01 89 FE'
+
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
+    i = 0;
+    # while True:
+
     s.sendall(bytes.fromhex(sendDataStr))
     data = s.recv(1024)
 
-print(type(data))
-print('Received:', data.hex())
+    # print(type(data))
+    i = i+1
+    print('Received:', data.hex(), 'i: ', i)
+
